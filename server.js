31f48db371ebe0d74a96dbef9eb80b99144fad1d
bhaumikpanchall <bhaumikpanchal2003@gmail.com>
2023-01-02 18:44:18 +0530

    Alert box added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -16,6 +18,19 @@ const CityRoutes = require("./routes/city");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+  session({
+    secret: "homeservice",
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.message = req.flash();
+  next();
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
